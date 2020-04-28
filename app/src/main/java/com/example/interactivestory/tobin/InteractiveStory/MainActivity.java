fe8f5c08838d359826565f426ab28b189d88d5c8
tobin <tobinthadathil@gmail.com>
2020-04-28 16:22:38 -0400

    initial Commit

---
+++ app/src/main/java/com/example/interactivestory/tobin/InteractiveStory/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.interactivestory.tobin.InteractiveStory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
